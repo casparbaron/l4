7f7f2013dc969eabe7edea4bdd174832591d72ed
Caspar <casparbaron@gmail.com>
2023-06-03 08:35:20 +0200

    V1.7

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import  Skills  from './skills'
 
 function Overlay() {
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '120px', }}>
       <a style={{ position: 'absolute', top: 40, left: 90, fontSize: '13px' }}>
         lfour
         <br />
         berlin creative agency
       </a>
-      <div style={{ position: 'absolute', top: 40, right: 40, fontSize: '13px' }}>get in contact</div>
+      <div style={{ position: 'absolute', top: 40, right: 40, fontSize: '13px' }}>
+        get in contact</div>
     </div>
   )
 }
