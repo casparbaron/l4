767d153fc724a2488fa292449d7380ce551616e1
Caspar <casparbaron@gmail.com>
2023-06-06 20:43:38 +0200

    V1.9

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function Items() {
 export const App = () => (
   <Canvas orthographic camera={{ zoom: 80 }} gl={{ alpha: false, antialias: false, stencil: false, depth: false }} dpr={[1, 1.5]}>
     <color attach="background" args={['#f0f0f0']} />
-
+  
     <ScrollControls damping={5} pages={5.6}>
       <Items />
       <Scroll html style={{ width: '100%' }}>
