a9ea55b6e3a0fcce9b87eeb6d799d7db1fc8aa6c
Caspar <casparbaron@gmail.com>
2023-06-19 21:19:35 +0200

    V1.9

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import { createRoot } from 'react-dom/client';
-import { Suspense, useState } from 'react';
-import Logo from './logo.png';
-import './styles.css';
-import { App } from './App';
-import Footer from './footer';
-import PopupComponent from './widget';
+import { createRoot } from 'react-dom/client'
+import { Suspense } from 'react'
+import Logo from './logo.png'
+import './styles.css'
+import { App } from './App'
+import Footer from './footer'
+import PopupComponent from './widget'
+
+const reloadPage = () => {
+  document.body.classList.add('fade-out');
+  setTimeout(() => {
+    window.location.reload();
+  }, 500);
+
+
+}
 
 createRoot(document.getElementById('root')).render(
   <>
@@ -14,36 +23,10 @@ createRoot(document.getElementById('root')).render(
 
     <PopupComponent />
 
-    <img src={Logo} style={{ position: 'absolute', top: 40, left: 40, width: 30 }} />
+    <img src={Logo} style={{ position: 'absolute', top: 40, left: 40, width: 30, cursor: 'pointer' }} onClick={reloadPage} />
 
     <Footer />
 
-    {/* Add the scroll-to-top button */}
-    <button
-      onClick={() => {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
-        });
-      }}
-      style={{
-        position: 'fixed',
-        bottom: 20,
-        right: 20,
-        width: 40,
-        height: 40,
-        borderRadius: '50%',
-        backgroundColor: 'blue',
-        color: 'white',
-        border: 'none',
-        cursor: 'pointer',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
-      }}
-    >
-      ↑
-    </button>
+   
   </>
-);
+)
